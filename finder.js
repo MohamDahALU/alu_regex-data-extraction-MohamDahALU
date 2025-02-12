c9c65dc378d06f7a145bdcbf43062fc0cda03c0e
MohamDahALU <m.dahab@alustudent.com>
2025-02-12 16:49:11 +0200

    improve presentation of console.logs

--- finder.js
+++ finder.js
@@ -2,32 +2,32 @@ import text from "./data.js";
 
 const emailRegex = /(?<= )[^ ]+@.+?(?= )/g
 const emails = text.match(emailRegex)
-console.log(emails)
+console.log({emails})
 
 const urlRegex = /http.+?(?= )/g
 const urls = text.match(urlRegex)
-console.log(urls)
+console.log({urls})
 
 const phoneRegex = /\(?\d{3}\)?[\.\- ]\d{3}[\.\- ]\d{4}/g
 const phones = text.match(phoneRegex)
-console.log(phones)
+console.log({"Phone numbers": phones})
 
 const cardRegex = /\d{4}([\- ])\d{4}\1\d{4}\1\d{4}/g
 const cards = text.match(cardRegex)
-console.log(cards)
+console.log({"Credit card numbers": cards})
 
 const timeRegex = /\d{1,2}\:\d{1,2}( [AP]M)?/g
 const times = text.match(timeRegex)
-console.log(times)
+console.log({Time: times})
 
 const htmlRegex = /<.+?>/g
 const htmls = text.match(htmlRegex)
-console.log(htmls)
+console.log({"HTML tags": htmls})
 
 const hashRegex = /#.+?(?= )/g
 const hashes = text.match(hashRegex)
-console.log(hashes)
+console.log({Hashtags: hashes})
 
 const currRegex = /\$[1-9\.\,]+/g
 const currs = text.match(currRegex)
-console.log(currs)
+console.log({"Currency amounts": currs})
